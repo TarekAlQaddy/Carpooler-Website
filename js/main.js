dc046ab6662f605313f2d5b52e0f9fffb1ecd32a
tarekalqaddy <tarek.alqaddy@gmail.com>
2017-05-10 03:05:24 +0200

    add how it works section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 /**
  * Created by Tarek AlQaddy on 5/6/2017.
  */
-// add navbar init
+//TODO: add navbar init
+var par = $('#how-it-works');
+var offset = (window.innerHeight-500)/2;
+$('#iphone').stick_in_parent({
+    parent: par,
+    offset_top: offset
+});
+
+
+
+var controller = new ScrollMagic.Controller();
+
+// new ScrollMagic.Scene({triggerElement:'#sec1', duration: 1600, offset: 400}).setPin('#iphone').addTo(controller);
+
+new ScrollMagic.Scene({triggerElement:'#sec2'}).setClassToggle('#screen2', 'visible').addTo(controller);
+new ScrollMagic.Scene({triggerElement:'#sec3'}).setClassToggle('#screen3', 'visible').addTo(controller);
