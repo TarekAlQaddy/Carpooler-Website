71bf0937d757f790b9f83feb9c0dacc355cc4107
tarekalqaddy <tarek.alqaddy@gmail.com>
2017-07-01 19:36:17 +0200

    why carpooler section

--- js/main.js
+++ js/main.js
@@ -2,18 +2,43 @@
  * Created by Tarek AlQaddy on 5/6/2017.
  */
 //TODO: add navbar init
-var par = $('#how-it-works');
-var offset = (window.innerHeight-500)/2;
-$('#iphone').stick_in_parent({
-    parent: par,
-    offset_top: offset
-});
+
+
+$(document).ready(function () {
+    var par = $('#how-it-works');
+    var offset = (window.innerHeight-500)/2;
+    $('#iphone').stick_in_parent({
+        parent: par,
+        offset_top: offset
+    });
 
 
 
-var controller = new ScrollMagic.Controller();
+    var controller = new ScrollMagic.Controller();
 
 // new ScrollMagic.Scene({triggerElement:'#sec1', duration: 1600, offset: 400}).setPin('#iphone').addTo(controller);
 
-new ScrollMagic.Scene({triggerElement:'#sec2'}).setClassToggle('#screen2', 'visible').addTo(controller);
-new ScrollMagic.Scene({triggerElement:'#sec3'}).setClassToggle('#screen3', 'visible').addTo(controller);
+    new ScrollMagic.Scene({triggerElement:'#sec2'}).setClassToggle('#screen2', 'visible').addTo(controller);
+    new ScrollMagic.Scene({triggerElement:'#sec3'}).setClassToggle('#screen3', 'visible').addTo(controller);
+
+
+    var box1 = document.getElementById('box1');
+    var box2 = document.getElementById('box2');
+    var box3 = document.getElementById('box3');
+    var why = document.getElementById('why-carpooler');
+    var height = why.clientHeight;
+
+    new ScrollMagic.Scene({triggerElement: why, duration: height, offset: -50})
+        .setTween(TweenMax.from(box1, 1, {x:-100, y:100}))
+        .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: why, duration: height, offset: -50})
+        .setTween(TweenMax.from(box2, 1, {x:150, y:-150}))
+        .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: box2, duration: height, offset: -50})
+        .setTween(TweenMax.from(box3, 1, {x:200,y:100}))
+        .addTo(controller)
+
+
+});
